61c4f7eedce42f8b6454d822418cbf92cd3ff05f
rservidornltd <rogelio.servidor@nltechdev.com>
2025-09-25 03:51:15 +0800

    apply onAuthStateChanged

--- react-practice/src/routes/navigation/navigation.component.jsx
+++ react-practice/src/routes/navigation/navigation.component.jsx
@@ -1,26 +1,41 @@
-import { Link, Outlet } from "react-router-dom";
-import CrwnLogo from '../../assets/crown.svg?react';
-import './navigation.styles.scss';
+import { Fragment, useContext } from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
-const Navigation = () =>{
-  return(
-    <>
-        <div className="navigation">
-            <Link className="logo-container" to='/'>
-                <CrwnLogo className="logo"/>
-            </Link>
-            <div className="nav-links-container">
-                <Link className="nav-link" to='/shop'>
-                    Shop
+import CrwnLogo from '../../assets/crown.svg?react';
+
+import './navigation.styles.scss';
+import { UserContext } from '../../contexts/user.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+const Navigation = () => {
+    const {currentUser} = useContext(UserContext);
+
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+          {
+            currentUser ? (
+                <span className='nav-link' onClick={signOutUser}>
+                        SIGN OUT
+                </span>
+            ):(
+                <Link className='nav-link' to='/auth'>
+                    SIGN IN
                 </Link>
-                <Link className="nav-link" to='/sign-in'>
-                    Sign In
-                </Link>
-            </div>
+            )
+          }
         </div>
-        <Outlet/>
-    </>
-  )
-}
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
 export default Navigation;
--- react-practice/src/utils/firebase/firebase.utils.js
+++ react-practice/src/utils/firebase/firebase.utils.js
@@ -1,9 +1,14 @@
+
 import { initializeApp } from 'firebase/app';
 import {
   getAuth,
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -18,18 +23,26 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: 'select_account',
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
   const userDocRef = doc(db, 'users', userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
@@ -43,6 +56,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInformation,
       });
     } catch (error) {
       console.log('error creating the user', error.message);
@@ -50,4 +64,23 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   }
 
   return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback)=> {
+
+  return onAuthStateChanged(auth, callback);
 };
