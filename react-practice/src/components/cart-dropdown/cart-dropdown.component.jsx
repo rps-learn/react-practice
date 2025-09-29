91ffbe70f0a2675e9e491c4ff6a0ba58d03b5bf1
rservidornltd <rogelio.servidor@nltechdev.com>
2025-09-30 02:26:16 +0800

    updated cart

--- react-practice/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ react-practice/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,28 @@
 import './cart-dropdown.styles.scss'
 import Button from '../button/button.component';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+import CartItem from '../cart-item/cart-item.component';
+import { useNavigate } from 'react-router-dom';
 
 const CartDropdown = () => {
+    const {cartItems} = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+    }
+
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items'/>
-            <Button>
+            <div className='cart-items'>
+                {
+                    cartItems.map((cartItem) => (
+                        <CartItem key={cartItem.id} cartItem={cartItem}/>
+                    ))
+                }
+            </div>
+            <Button onClick={goToCheckoutHandler}>
                 Go to checkout
             </Button>
         </div>
